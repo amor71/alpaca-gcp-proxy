d5a9c4f5094f686423548d68f028bbf13713bd64
Amichay Oren <amichay@appcard.com>
2023-03-31 17:24:42 +0300

    firestore 3

--- events/new_user.py
+++ events/new_user.py
@@ -6,5 +6,11 @@ def new_user_handler(payload: dict):
 
     db = firestore.Client()
 
+    print("db", db)
+
     doc_ref = db.collection("users").document("amichay")
-    doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+
+    print("doc_ref", doc_ref)
+    status = doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+
+    print("status=", status)
