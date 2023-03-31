247939ea691f41e23530152578ecd24315adbcb1
Amichay Oren <amichay@appcard.com>
2023-03-31 09:40:53 +0300

    connection 1

--- events/new_user.py
+++ events/new_user.py
@@ -1,2 +1,10 @@
+import psycopg
+
+
 def new_user_handler(payload: dict):
     print("payload=", payload)
+
+    with psycopg.connect(
+        "username=admin password=admin database=postgres unix_sock=/cloudsql/development-380917:us-central1:customers/.s.PGSQL.5432"
+    ) as conn:
+        print("connection", conn)
