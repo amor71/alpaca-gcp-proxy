ad41af796309328deb64bd880702afd975f0a6c9
Amichay Oren <amichay@appcard.com>
2023-04-22 15:27:43 +0300

    LB : wip

--- main.tf
+++ main.tf
@@ -107,9 +107,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+  location    = google_cloud_run_service.react.location
+  project     = google_cloud_run_service.react.project
+  service     = google_cloud_run_service.react.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
