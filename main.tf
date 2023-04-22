66374d13bb8e3472aff3ae1862e339bedf15b0ca
Amichay Oren <amichay@appcard.com>
2023-04-22 14:17:12 +0300

    TF : cloud run no auth

--- main.tf
+++ main.tf
@@ -96,3 +96,20 @@ resource "google_cloud_run_service" "default" {
     latest_revision = true
   }
 }
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
