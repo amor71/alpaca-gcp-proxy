b6c90b86e6dc8c3859d5753ce6ac91e81953b98f
Amichay Oren <amichay@appcard.com>
2023-04-30 16:12:32 +0300

    wip

--- apigateway.tf
+++ apigateway.tf
@@ -43,15 +43,18 @@ resource "google_api_gateway_api" "api_gw" {
 }
 
 resource "google_api_gateway_api_config" "api_gw_config" {
-  provider      = google-beta
-  api           = google_api_gateway_api.api_gw.api_id
-  api_config_id = "api-config"
+  provider = google-beta
+  api      = google_api_gateway_api.api_gw.api_id
+  #api_config_id = "api-config"
 
   openapi_documents {
     document {
       path     = "spec.yaml"
       contents = filebase64("./apigateway/openapi.yaml")
     }
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
