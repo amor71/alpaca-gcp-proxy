3b4e4f317aceaf9fa493c6f19829b46820ee65b5
Amichay Oren <amichay@appcard.com>
2023-04-30 22:38:30 +0300

    wip

--- main.py
+++ main.py
@@ -45,17 +45,17 @@ def proxy(request):
 
     if directories[0] in ["alpaca", "plaid", "stytch", "bank"]:
         # Set CORS headers for the preflight request
-        if request.method == "OPTIONS":
-            # Allows GET requests from any origin with the Content-Type
-            # header and caches preflight response for an 3600s
-            headers = {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Max-Age": "3600",
-            }
-
-            return ("", 204, headers)
+        # if request.method == "OPTIONS":
+        #    # Allows GET requests from any origin with the Content-Type
+        #    # header and caches preflight response for an 3600s
+        #    headers = {
+        #        "Access-Control-Allow-Origin": "*",
+        #        "Access-Control-Allow-Methods": "*",
+        #        "Access-Control-Allow-Headers": "*",
+        #        "Access-Control-Max-Age": "3600",
+        #    }
+        #
+        #    return ("", 204, headers)
 
         try:
             t = time()
