e21dd0e8c125a6a7e70a6c024128a4fb27ee2134
Amichay Oren <amichay@appcard.com>
2023-04-30 22:57:08 +0300

    wip

--- main.py
+++ main.py
@@ -96,6 +96,6 @@ def proxy(request):
         }
         headers = {**cors_headers, **r.headers}
         print("headers", headers)
-        return (r.text, r.status_code, r.headers.items())
+        return (r.raw, r.status_code, r.headers.items())
 
     return ("proxy not found", 400)
