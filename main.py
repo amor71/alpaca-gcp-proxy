3196841c38e96c04cea1c3ab715cd283a3323f0f
Amichay Oren <amichay@appcard.com>
2023-05-02 17:26:05 +0300

    wip

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def _keep_header(header: str) -> bool:
         "Function-Execution-Id",
         "User-Agent",
         "Host",
-        # "Transfer-Encoding",
+        "Transfer-Encoding",
     ]
 
     return all(x not in header for x in headers_to_remove)
