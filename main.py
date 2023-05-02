6bedb72dfb20473ef120b9d70571207fbd09fdbc
Amichay Oren <amichay@appcard.com>
2023-05-02 21:49:53 +0300

    support OPTIONS for link

--- main.py
+++ main.py
@@ -104,6 +104,18 @@ def proxy(request):
                     headers,
                 )
             elif directories[1] == "link":
+                if request.method == "OPTIONS":
+                    # Allows GET requests from any origin with the Content-Type
+                    # header and caches preflight response for an 3600s
+                    headers = {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "*",
+                        "Access-Control-Allow-Headers": "*",
+                        "Access-Control-Max-Age": "3600",
+                    }
+
+                    return ("", 204, headers)
+
                 try:
                     r = link(request, headers)
                 except HTTPError as e:
