cc4a9851b6455ac9c636f9a34958b4d60cb1c792
Amichay Oren <amichay@appcard.com>
2023-05-05 13:04:51 +0300

    wip

--- auth.py
+++ auth.py
@@ -24,7 +24,7 @@ def is_token_invalid(token: str, headers: dict) -> bool:
     encoded_token = base64.b64encode(bytes(token, "utf-8"))
     encoded_token_str = encoded_token.decode("utf-8")
     print("encoded", encoded_token_str)
-    payload = {"session_token": encoded_token_str}
+    payload = {"session_token": token}
     r = stytch_proxy(
         method="POST",
         url="v1/sessions/authenticate",
