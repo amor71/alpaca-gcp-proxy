abf00b6fe06e71754d6fa0e9f1d5287d82ab1ce9
Amichay Oren <amichay@appcard.com>
2023-05-06 10:44:16 +0300

    gw - wip

--- apigateway.tf
+++ apigateway.tf
@@ -35,3 +35,56 @@ resource "google_api_gateway_gateway" "api_gw_gw" {
   gateway_id = "api-gateway"
 }
 
+#---------------
+# LB
+#---------------
+
+resource "google_compute_region_network_endpoint_group" "gw_neg_us" {
+  name                  = "neg-gw"
+  provider              = google-beta
+  network_endpoint_type = "SERVERLESS"
+  region                = us-east4
+  serverless_deployment {
+    platform = "apigateway.googleapis.com"
+    resource = google_api_gateway_gateway.api_gw_gw.name
+  }
+}
+
+module "lb-http" {
+  source  = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
+  version = "~> 4.5"
+
+  project = var.project_id
+  name    = "app"
+
+  managed_ssl_certificate_domains = ["api.nine30.com"]
+  ssl                             = true
+  https_redirect                  = true
+
+  backends = {
+    default = {
+      groups = [
+        {
+          group = google_compute_region_network_endpoint_group.gw_neg_us.id
+        }
+      ]
+
+      enable_cdn = false
+
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+
+      description            = null
+      custom_request_headers = null
+      security_policy        = null
+    }
+  }
+}
