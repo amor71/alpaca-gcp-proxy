ec7a716aa45585ba85960f8fdb495b0fba98f4ba
Amichay Oren <amichay@appcard.com>
2023-05-07 23:00:27 +0300

    wip

--- apigateway.tf
+++ apigateway.tf
@@ -45,7 +45,7 @@ resource "google_compute_region_network_endpoint_group" "gw_neg_us" {
   network_endpoint_type = "SERVERLESS"
   region                = var.region
 
-  serverless_deployment {
+  serverlessDeployment {
     platform = "apigateway.googleapis.com"
     resource = google_api_gateway_gateway.api_gw_gw.id
   }
