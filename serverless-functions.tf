56f66267364e5e229b83d0d1557e0e3a5b2eb68e
Amichay Oren <amichay@appcard.com>
2023-05-20 13:32:28 +0300

    wip

--- serverless-functions.tf
+++ serverless-functions.tf
@@ -119,12 +119,6 @@ resource "google_cloudfunctions_function" "proxy" {
   }
 }
 
-resource "google_cloudfunctions_function_iam_policy" "proxy_policy" {
-  project        = google_cloudfunctions_function.proxy.project
-  region         = google_cloudfunctions_function.proxy.region
-  cloud_function = google_cloudfunctions_function.proxy.name
-  policy_data    = data.google_iam_policy.n30-noauth.policy_data
-}
 
 # --------------------------
 # -- Slack Notifier Function
