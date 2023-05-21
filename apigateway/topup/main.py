d7239cbe19dfa779ef1135f113d52e91724d4108
Amichay Oren <amichay@appcard.com>
2023-05-21 17:49:02 +0300

    wip

--- apigateway/topup/main.py
+++ apigateway/topup/main.py
@@ -6,5 +6,13 @@ from infra import auth  # type: ignore
 @functions_framework.http
 @auth
 def topup(request):
-    print("topup", request)
+    user_id = request.args.get("userId")
+
+    print(request)
+
+    # Use the user_id parameter in your function logic
+    if user_id:
+        # Do something with the user_id
+        print(f"Received user_id: {user_id}")
+
     return ("OK", 200)
