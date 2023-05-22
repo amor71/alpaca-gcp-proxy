823caecc7e555a91a00e78aa8b77ee8c7ebe72fd
Amichay Oren <amichay@appcard.com>
2023-05-22 12:51:36 +0300

    implement topup API validation

--- apigateway/topup/main.py
+++ apigateway/topup/main.py
@@ -1,18 +1,51 @@
+from enum import Enum
+
 import functions_framework
 
 from infra import auth  # type: ignore
 
 
+class Frequency(Enum):
+    Weekly = 1
+    Monthly = 2
+
+
+def process(amount: int, frequency: Frequency) -> None:
+    print(f"process: {amount}, {frequency}")
+
+
 @functions_framework.http
 @auth
 def topup(request):
+    """Implement PUT /users/{userId}/topup end-point"""
+
     user_id = request.args.get("userId")
+    print(f"Received user_id: {user_id}")
 
-    print(request)
+    # validate API
+    payload = request.get_json() if request.is_json else None
 
-    # Use the user_id parameter in your function logic
-    if user_id:
-        # Do something with the user_id
-        print(f"Received user_id: {user_id}")
+    if (
+        not payload
+        or not (amount_str := payload.get("amount"))
+        or not (frequency_str := payload.get("frequency"))
+    ):
+        return (
+            "Missing or invalid payload (expected JSON w/ 'amount' and 'frequency')",
+            400,
+        )
+
+    try:
+        amount = int(amount_str)
+        assert amount > 0
+    except Exception:
+        return ("amount needs to be positive int", 400)
+
+    try:
+        frequency = Frequency[frequency_str]
+    except Exception:
+        return ("frequency needs to be 'Weekly' or 'Monthly'", 400)
+
+    process(amount, frequency)
 
     return ("OK", 200)
