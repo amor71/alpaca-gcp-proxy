2e6af574aebb17e0e533413d514a82a2a7562e93
Amichay Oren <amichay@appcard.com>
2023-05-27 10:34:09 +0300

    openai - skeleton

--- apigateway/chatbot/main.py
+++ apigateway/chatbot/main.py
@@ -1,11 +1,44 @@
 import functions_framework
+import openai
+from google.cloud import secretmanager  # type:ignore
 
-from infra import auth  # type: ignore
+from infra import auth  # type:ignore
+from infra.config import project_id  # type:ignore
+from infra.proxies.proxy_base import check_crc  # type:ignore
+
+api_key = "openai_api_key"
+org_id = "openai_org_id"
+
+
+def _get_credentials() -> tuple:
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Access the secret version.
+    api_key_response = client.access_secret_version(
+        request={
+            "name": f"projects/{project_id}/secrets/{api_key}/versions/latest"
+        }
+    )
+    org_id_response = client.access_secret_version(
+        request={
+            "name": f"projects/{project_id}/secrets/{org_id}/versions/latest"
+        }
+    )
+
+    check_crc(api_key_response)
+    check_crc(org_id)
+    return (
+        api_key_response.payload.data.decode("UTF-8"),
+        org_id_response.payload.data.decode("UTF-8"),
+    )
 
 
 @functions_framework.http
 @auth
 def chatbot(request):
     """Implement POST /v1/chatbot"""
+
+    openai.api_key, openai.organization = _get_credentials()
+    print(openai.Model.list())
 
     return ("OK", 200)
