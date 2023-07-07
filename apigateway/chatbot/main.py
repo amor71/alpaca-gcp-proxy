ad81dcc1e7bc3c5d7a17fecb80bd6d7b92687989
Amichay Oren <amichay@appcard.com>
2023-07-07 09:56:42 +0300

    change chatbot prompt, test function call

--- apigateway/chatbot/main.py
+++ apigateway/chatbot/main.py
@@ -46,11 +46,38 @@ def chatbot(request):
 
     openai.api_key, openai.organization = _get_credentials()
 
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant. You answer questions as a 25 year old financial professional. You are funny, but never sarcastic. You are using a voice and language that fits young people ages 18 to 26 years old.",
+        },
+        {"assistant": "\n\nI'm Hushi, how may I assist you today?"},
+        {"role": "user", "content": f"{question}"},
+    ]
+    functions = [
+        {
+            "name": "get_portfolio_details",
+            "description": "Get the list of equities in my portfolio",
+        }
+    ]
     chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": f"{question}"}],
+        model="gpt-4-0613",
+        messages=messages,
+        functions=functions,
+        function_call="auto",
     )
-    answer = chat_completion.choices[0].message.content
+
+    print(chat_completion)
+
+    response_message = chat_completion["choices"][0]["message"]
+
+    if response_message.get("function_call"):
+        function_name = response_message["function_call"]["name"]
+
+        answer = f"call the function {function_name}"
+    else:
+        answer = response_message.content
+
     payload: dict = {"answer": answer}
 
     return (payload, 200)
