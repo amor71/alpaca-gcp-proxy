3d20e9ac4808032f242146f14e2196159d387c15
Amichay Oren <amichay@appcard.com>
2023-07-27 14:11:49 +0300

    pull user_id from auth token

--- apigateway/new_user/main.py
+++ apigateway/new_user/main.py
@@ -2,10 +2,19 @@ import functions_framework
 from new_user import new_user_handler
 
 from infra import auth  # type: ignore
+from infra.logger import log_error
 
 
 @functions_framework.http
 @auth
 def new_user(request):
+    user_id = authenticated_user_id.get()  # type: ignore
+    if not user_id:
+        log_error(
+            "new_user()",
+            "could not load authenticated user_id, this shouldn't have happened",
+        )
+        return ("Something went wrong", 403)
+
     new_user_handler(user_id)
     return ("OK", 200)
