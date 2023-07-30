e78de0843575bf6ff4a9eeb9a330f3969114355f
Amichay Oren <amichay@appcard.com>
2023-07-30 19:08:33 +0300

    fix data storage bug

--- infra/infra/data/preferences.py
+++ infra/infra/data/preferences.py
@@ -7,9 +7,8 @@ class Preferences:
     @classmethod
     def add(cls, user_id: str, data: dict) -> None:
         db = firestore.Client()
-        doc = db.collection("preferences").document(user_id).get()
-
-        if doc.exists:
-            doc.update(data)
+        doc_ref = db.collection("preferences").document(user_id)
+        if doc_ref.get().exists:
+            doc_ref.update(data)
         else:
-            doc.set(data)
+            doc_ref.set(data)
