db1a4186839d8de378adc2e6de8ab0650883dcfa
Amichay Oren <amichay@appcard.com>
2023-07-30 18:40:03 +0300

    forgot to add file to infra before building..

---
+++ infra/infra/data/preferences.py
@@ -0,0 +1,15 @@
+from google.cloud import firestore  # type: ignore
+
+from ..logger import log_error
+
+
+class Preferences:
+    @classmethod
+    def add(cls, user_id: str, data: dict) -> None:
+        db = firestore.Client()
+        doc = db.collection("preferences").document(user_id)
+
+        if doc.exists:
+            doc.update(data)
+        else:
+            doc.set(data)
