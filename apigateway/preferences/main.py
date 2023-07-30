deb95ed7d94c6eccce96d8124a737a6b68110b98
Amichay Oren <amichay@appcard.com>
2023-07-30 18:02:13 +0300

    bug-fix

--- apigateway/preferences/main.py
+++ apigateway/preferences/main.py
@@ -5,5 +5,5 @@ from infra import auth, authenticated_user_id  # type: ignore
 
 @functions_framework.http
 @auth
-def missions(request):
+def preferences(request):
     """Implement PATCH /v1/users -> update preferences"""
