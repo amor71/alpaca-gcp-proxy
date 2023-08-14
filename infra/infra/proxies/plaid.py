43d1c07d44dcc78e99ed84bd1fe8091de764de5c
Amichay Oren <amichay@appcard.com>
2023-08-14 14:30:45 +0300

    added logging

--- infra/infra/proxies/plaid.py
+++ infra/infra/proxies/plaid.py
@@ -70,7 +70,7 @@ def plaid_proxy(
     if payload:
         payload.update(auth)
 
-    print(f"plaid_proxy to {request_url}")
+    print(f"plaid_proxy {method} {request_url} with {payload}")
     return request(
         method=method,
         url=request_url,
