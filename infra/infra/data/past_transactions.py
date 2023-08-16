d9baf6094be319f83691173ab92e15641fdb815a
Amichay Oren <amichay@appcard.com>
2023-08-16 22:34:54 +0300

    bug-fix: check if transactions document exists, before updating

--- infra/infra/data/past_transactions.py
+++ infra/infra/data/past_transactions.py
@@ -4,9 +4,15 @@ from google.cloud import firestore
 def save_past_transactions(user_id: str, cursor: str, data: dict) -> None:
     print(f"save_past_transactions: {user_id}:{data}")
 
+    cursor_payload = {"cursor": cursor}
     db = firestore.Client()
     doc_ref = db.collection("plaid_transactions").document(user_id)
-    doc_ref.update({"cursor": cursor})
+    doc = doc_ref.get()
+    if not doc.exists:
+        doc_ref.set(cursor_payload)
+    else:
+        doc_ref.update(cursor_payload)
+
     new_transactions = doc_ref.collection("transactions").document()
     new_transactions(data)
 
