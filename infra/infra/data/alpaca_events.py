1737747e179a5886170e6972d55384eb2771d8b2
Amichay Oren <amichay@appcard.com>
2023-08-19 12:35:22 +0300

    change event storage collection structure

--- infra/infra/data/alpaca_events.py
+++ infra/infra/data/alpaca_events.py
@@ -6,13 +6,13 @@ class AlpacaEvents:
     def add(cls, event_entity: str, event_id: str) -> None:
         data = {"updated_at": firestore.SERVER_TIMESTAMP, "event_id": event_id}
         db = firestore.Client()
-        db.collection("alpaca_events").collection(event_entity).add(data)
+        db.collection(f"alpaca_events_{event_entity}").add(data)
 
     @classmethod
     def latest_event_id(cls, event_entity: str) -> str | None:
         db = firestore.Client()
 
-        collection = db.collection("alpaca_events").collection(event_entity)
+        collection = db.collection(f"alpaca_events_{event_entity}")
         query = collection.order_by(
             "updated_at", direction=firestore.Query.DESCENDING
         ).limit(1)
