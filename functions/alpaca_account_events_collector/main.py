50038d4b13fdbb811944f49277e878c4745f4a17
Amichay Oren <amichay@appcard.com>
2023-08-23 14:11:39 +0300

    load latest alpaca account event

--- functions/alpaca_account_events_collector/main.py
+++ functions/alpaca_account_events_collector/main.py
@@ -1,14 +1,13 @@
-import base64
-import json
-
 import functions_framework
 from cloudevents.http.event import CloudEvent
+
+from infra.infra.data.alpaca_events import AlpacaEvents
 
 
 @functions_framework.cloud_event
 def alpaca_account_events_collector(cloud_event: CloudEvent):
-    message = json.loads(
-        base64.b64decode(cloud_event.data["message"]["data"]).decode("utf-8")
-    )
+    print("alpaca_account_events_collector()")
 
-    print(f"alpaca_account_events_collector(): {message}")
+    data = AlpacaEvents.latest_event_id("accounts")
+
+    print("data=", data)
