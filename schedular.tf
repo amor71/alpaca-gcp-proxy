8c46e02d860212bc018f7fa1804736d62b611078
Amichay Oren <amichay@appcard.com>
2023-08-23 13:34:01 +0300

    fix typo

--- schedular.tf
+++ schedular.tf
@@ -7,7 +7,7 @@ resource "google_cloud_scheduler_job" "alpaca_account_events_collector" {
   schedule = "0 * * * *" # This runs every hour, adjust as needed.
 
   pubsub_target {
-    topic_name = google_pubsub_topic.scheduler_topic.name
+    topic_name = google_pubsub_topic.alpaca_account_events.name
     data       = base64encode("Your message data here")
   }
 }
