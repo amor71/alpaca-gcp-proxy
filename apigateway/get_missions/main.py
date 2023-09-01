b4031a067fed855cbd4c407c9d2974c4028afef1
Amichay Oren <amichay@appcard.com>
2023-09-01 12:56:15 +0300

    missions wip

--- apigateway/get_missions/main.py
+++ apigateway/get_missions/main.py
@@ -1,10 +1,9 @@
 import functions_framework
-from google.cloud import firestore  # type: ignore
+from flask import abort
 
 from infra import auth, authenticated_user_id  # type: ignore
+from infra.infra.data.missions import Missions
 from infra.logger import log_error
-from infra.plaid_actions import load_identities
-from infra.stytch_actions import get_from_user_vault
 
 
 @functions_framework.http
@@ -19,5 +18,17 @@ def get_missions(request):
             "could not load authenticated user_id, this shouldn't have happened",
         )
         return ("Something went wrong", 403)
+
+    missions = Missions(user_id=user_id)
+
+    payload = []
+    for mission in missions:
+        print(mission)
+
+    if not payload:
+        log_error(
+            "get_missions()", f"{user_id} does not have any active missions"
+        )
+        abort(400)
 
     return ("OK", 200)
