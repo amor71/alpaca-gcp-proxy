cbba61b09095d8b410c9d792bc78245846e32902
Amichay Oren <amichay@appcard.com>
2023-09-01 12:31:20 +0300

    improve logging

--- infra/infra/auth.py
+++ infra/infra/auth.py
@@ -13,7 +13,6 @@ def get_bearer_token(request: Request) -> str | None:
     """Extract 'Bearer' token from Authorization header"""
     if auth_header := request.headers.get(authorization_header):
         try:
-            print("get_bearer_token", auth_header)
             auth_headers = auth_header.split()
             if auth_headers[0] == "Bearer":
                 return auth_headers[1]
